c63503338187bdec216f4baa7928f8487d8cb5f2
leandroassad <leandroassad@hotmail.com>
2023-07-13 09:45:27 -0300

    Alteracao do Master

--- src/com/temp/Main.java
+++ src/com/temp/Main.java
@@ -2,10 +2,15 @@ package com.temp;
 
 public class Main {
 
+	public static void print(String str) {
+		System.out.println("<<< " + str + " >>>");
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Main Branch");
 
+		print("chamada do print");
 	}
 
 }
